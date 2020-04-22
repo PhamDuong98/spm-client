161269ca6d1e458d4245bb465c4e6a5692bfa1c0
dotrungduc <duc010298@gmail.com>
2020-04-22 22:32:22 +0700

    fix component

--- src/app/list-product/list-product.module.ts
+++ src/app/list-product/list-product.module.ts
@@ -4,9 +4,10 @@ import { ListProductRoutingModule } from './list-product-routing.module';
 import {ListProductComponent} from './list-product.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { AddProductDialogComponent } from './add-product-dialog/add-product-dialog.component';
 
 @NgModule({
-  declarations: [ListProductComponent],
+  declarations: [ListProductComponent, AddProductDialogComponent],
   imports: [
     CommonModule,
     ListProductRoutingModule,
