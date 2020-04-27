e5780e223b6b35921eeec3cfcb64f1028530f51c
dotrungduc <duc010298@gmail.com>
2020-04-27 22:36:19 +0700

    jwt function

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { RestService } from '../core/service/rest.service';
 
 @Component({
   selector: 'app-list-product',
@@ -8,28 +8,29 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./list-product.component.scss']
 })
 export class ListProductComponent implements OnInit {
+  allProductData: any;
 
   displayedColumns: string[] = ['No', 'barcode', 'name', 'importPrice', 'exportPrice', 'unit', 'category'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
+  constructor(
+    private restService: RestService
+  ) { }
+
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
+    this.getAllFromApi();
+  }
+
+  getAllFromApi() {
+    this.restService.getAllProduct()
+      .subscribe(
+        (data: any) => {
+          this.allProductData = data.data.allProducts;
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
   }
 }
-
-export interface PeriodicElement {
-  position: number;
-  barcode: string;
-  name: string;
-  importPrice: number;
-  exportPrice: number;
-  unit: string;
-  category: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, barcode: '12345', name: 'Kem đánh răng Amway', importPrice: 120000, exportPrice: 140000, unit: 'TUYP', category: 'Kem đánh răng' },
-  { position: 2, barcode: '12345', name: 'Kem đánh răng Amway', importPrice: 120000, exportPrice: 140000, unit: 'TUYP', category: 'Kem đánh răng' },
-];
